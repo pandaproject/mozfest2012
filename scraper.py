415e392e83c25aca7f644478a8d6cfdf7a7686d8
Christopher Groskopf <staringmonkey@gmail.com>
2012-11-09 16:34:04 +0000

    Iterating.

--- scraper.py
+++ scraper.py
@@ -7,56 +7,91 @@ import requests
 
 base_url = 'http://localhost:8000/'
 first_path = '2000s.html'
-scraped_paths = []
+scraped_urls = []
+output = []
 
-def get_page_doc(path):
-    if path not in scraped_paths:
-        scraped_paths.append(path)
-        response = requests.get(urljoin(base_url, path))
-        return fromstring(response.content)
+def scrape_decade_page(url):
+    if url not in scraped_urls:
+        scraped_urls.append(url)
+        response = requests.get(url)
     else:
         return None
 
-def scrape_decade_doc(doc):
+    doc = fromstring(response.content)
+
     year_links = doc.cssselect('tr b a')
     
     for year_link in year_links:
         path = year_link.attrib['href']
-       
-        year_doc = get_page_doc(path)
+        year_url = urljoin(base_url, path)
+        data = scrape_year_page(year_url)
 
-        if year_doc is not None:
-            scrape_year_doc(year_doc)
+        output.append(data)
 
     decade_links = doc.cssselect('td[colspan="4"] a')
 
     for decade_link in decade_links:
         path = decade_link.attrib['href']
+        decade_url = urljoin(base_url, path)
+        scrape_decade_page(decade_url)
 
-        decade_doc = get_page_doc(path)
+def scrape_year_page(url):
+    if url not in scraped_urls:
+        scraped_urls.append(url)
+        response = requests.get(url)
+    else:
+        return None
 
-        if decade_doc is not None:
-            scrape_decade_doc(decade_doc)
+    doc = fromstring(response.content)
 
-def scrape_year_doc(doc):
-    print doc.cssselect('h2')[0].text
+    data = {}
 
-response = requests.get(urljoin(base_url, first_path))
-doc = fromstring(response.content)
-scrape_decade_doc(doc)
+    data['year'] = doc.cssselect('h2')[0].text
+    
+    rows = doc.cssselect('table table tr')
 
-outline ="""
-request page
+    # Winners
+    cells = rows[0].cssselect('td')
+    data['male_winner'] = rows[1].text
+    data['female_winner'] = rows[3].text
 
-for page in pages:
-        for row in list:
-                get detail page
-                    
-                            get field 1 by id
-                                    get field 2 position relative to field 1
-                                            get field 3 and split into 4 and 5
-                                                    store detail url
-                                                        
-                                                        save to csv
-                                                        save to panda?
-                                                        """
+    # Registered runners
+    cells = rows[1].cssselect('td')
+    data['registered_runners'] = int(cells[1].text.replace(',', ''))
+
+    # Finished runners
+    cells = rows[2].cssselect('td')
+    data['finished_runners'] = int(cells[1].text.replace(',', ''))
+
+    # Temps
+    temp_items = rows[3].cssselect('li')
+    data['start_temp'] = int(temp_items[0].text.split()[-1])
+    data['middle_temp'] = int(temp_items[1].text.split()[-1])
+    data['end_temp'] = int(temp_items[2].text.split()[-1])
+
+    # Wind
+    cells = rows[4].cssselect('td')
+    wind = cells[1].text
+
+    if wind == 'calm':
+        data['wind_dir'] = None
+        data['wind_speed'] = 0
+    else:  
+        direction, speed = wind.split()
+
+        data['wind_dir'] = direction
+        data['wind_speed'] = int(speed)
+
+    # Sunshine
+    cells = rows[5].cssselect('td')
+    data['hours_of_sunshine'] = cells[1].text
+
+    # Weather description
+    data['weather'] = rows[6].cssselect('i')[0].text 
+
+    return data
+
+first_url = urljoin(base_url, first_path)
+scrape_decade_page(first_url)
+
+print output
